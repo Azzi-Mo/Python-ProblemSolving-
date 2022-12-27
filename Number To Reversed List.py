73465f8f3f6b2355cb7d528e6d6718c823b7b6e2
Azzi-Mo <alhwarymostafa@gmail.com>
2022-12-27 17:59:32 +0200

    edit

--- Number To Reversed List.py
+++ Number To Reversed List.py
@@ -1,12 +1,22 @@
 # print(convert(564987654)) # [4, 5, 6, 7, 8, 9, 4, 6, 5]
 # print(convert(529132)) # [2, 3, 1, 9, 2, 5]
 
+
+# solution 1
+
+# def convert(n):
+#  st = str(n)
+#  res = []
+#  for num in st:
+#       res.append(int(num))
+#  res.reverse()
+#  return res
+
+# solution 2
 def convert(n):
- st = str(n)
  res = []
- for num in st:
-      res.append(int(num))
- res.reverse()
+ for num in str(n):
+    res.insert(0,int(num))
  return res
 
 print(convert(529132))
